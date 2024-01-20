7f0c8f0fd63e8275b72320d4faf5b96784287f87
Ian Kitchen <ian@oceanrain.com>
2024-01-20 13:50:50 +0000

    rgb and random 25 tops

--- js/pyramids.js
+++ js/pyramids.js
@@ -10,21 +10,26 @@ function drawPyramids(){
     for(let i = 1; i<=9; i++){
         for(let j = 1; j<=6; j++){
             print(i + ' and ' + j);
-            fill(i*2,j*2.5,i+j,0.9) //Hue per column from 2 to 16, Sat increases towards botton, brightness towards bottom left
+            let hue = i*1.77; //1.77 to 16
+            let sat = j*2.5-2.5; //0 to 12.5
+            let lightness = (i+j)*0.9; //2 to 15
+            fill(hue,sat,lightness,1) 
     
-            x = i*95+10;
-            y = j*95;
+            x = i*95+3;
+            y = j*95-9;
             d = 95;
 
             doPyramid(x,y,d);
 
-            if (false){
+            if ((i-1)*6+j==54){
                 saveCanvas('pyramid' + i + j, 'png');
+            }
+
+            if(false){
                 fill(16);
                 rect(0,0,1050,742);
             }
 
-    
             print( 'x: ' + x + 'y: ' + y + 'd: ' + d);
         } 
     }
@@ -50,8 +55,8 @@ class pyramid {
       this.pointY3 = y+l;
       this.pointX4 = x + l;
       this.pointY4 = y + l;
-      this.pointX = x + l/2 + noise(x)*5;
-      this.pointY = y + l/2 + noise(y)*5;
+      this.pointX = x + l/2 + noise(x)*25;
+      this.pointY = y + l/2 + noise(y)*25;
       this.report = function () {
         print(this.pointX1 + " / " + this.pointY1 + " / " + this.pointX2 + " / " + this.pointY2);
       };
@@ -59,7 +64,7 @@ class pyramid {
   
     drawPyramid() {
         triangle(this.pointX1,this.pointY1, this.pointX2, this.pointY2, this.pointX, this.pointY); //top
-        fill(5,5,5,1);
+        fill(0,10,10,1); //fill(5,5,5,1);
         triangle(this.pointX1,this.pointY1, this.pointX3, this.pointY3, this.pointX, this.pointY); //left
         fill(10,10,10,1);
         triangle(this.pointX4,this.pointY4, this.pointX2, this.pointY2, this.pointX, this.pointY); //right
